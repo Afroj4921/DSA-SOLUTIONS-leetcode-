3bbba00f928e46cbe3bc2359d857eab0b9af2d0b
Afroj Ansari <ansariafroj0077@gmail.com>
2026-07-24 07:23:52 +0530

    Time: 2 ms (77.96%), Space: 7.7 MB (88.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 || n == 1){
-            return n;
+        int prev2 = 0, prev = 1;
+        if(n <= 0) return n;
+        for(int i=2; i<=n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return fib(n-1) + fib(n-2);
+        return prev;
     }
 };
