257b4268a0a5dff4f228e6272ca16ab5aa0635d7
Afroj Ansari <ansariafroj0077@gmail.com>
2026-08-05 07:13:04 +0530

    Time: 35 ms (54.25%), Space: 22.2 MB (49.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int f(int ind, int T, vector<int>& coins, vector<vector<int>>& dp){
-        if(ind == 0){
-            if(T % coins[0] == 0) return T/coins[0];
-            return 1e9;
-        }
-        if(dp[ind][T] != -1) return dp[ind][T];
-        int notPick = 0 + f(ind-1, T, coins, dp);
-        int pick = INT_MAX;
-        if(coins[ind] <= T) pick = 1 + f(ind, T-coins[ind], coins, dp);
-
-        return dp[ind][T] = min(pick, notPick);  
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = f(n-1, amount, coins, dp);
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        for(int T=0; T<=amount; T++){
+            if(T % coins[0] == 0) prev[T] = T/coins[0];
+            else prev[T] = 1e9;
+        }
+
+        for(int ind = 1; ind<n; ind++){
+            for(int T=0; T<=amount; T++){
+                int notPick = 0 + prev[T];
+                int pick = INT_MAX;
+                if(coins[ind] <= T) pick = 1 + curr[T-coins[ind]];
+
+                curr[T] = min(pick, notPick);
+            }
+            prev = curr;
+        }
+
+        int ans = prev[amount];
 
         if(ans >= 1e9) return -1;
         else return ans;
