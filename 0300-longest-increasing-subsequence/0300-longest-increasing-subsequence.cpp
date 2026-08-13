2a8e9312395da528ef9a4cb440ae5dab345ccb13
Afroj Ansari <ansariafroj0077@gmail.com>
2026-08-13 11:05:00 +0530

    Time: 0 ms (100%), Space: 14.2 MB (71.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // recurssion and memoization
     int f(int ind, int prev_ind, vector<int>& nums, int n, vector<vector<int>>& dp){
         if(ind == n) return 0;
         if(dp[ind][prev_ind+1] != -1) return dp[ind][prev_ind+1];
@@ -12,18 +13,18 @@ public:
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> next(n+1, 0), curr(n+1, 0);
-
-        for(int ind=n-1; ind>=0; ind--){
-            for(int prev_ind=ind-1; prev_ind>=-1; prev_ind--){
-                int len = 0 + next[prev_ind+1];
-                if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-                    len = max(len, 1 + next[ind+1]);
-                }
-                curr[prev_ind+1] = len;
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len = 1;
+        for(int i=1; i<n; i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            } else{
+                auto ind = lower_bound(temp.begin(), temp.end(), nums[i]);
+                *ind = nums[i];
             }
-            next = curr;
         }
-        return next[-1+1];
+        return len;
     }
 };
