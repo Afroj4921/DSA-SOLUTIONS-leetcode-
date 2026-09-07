3cc303f0401d887918c7066bafb5ac885710d4b8
Afroj Ansari <ansariafroj0077@gmail.com>
2026-09-07 18:15:21 +0530

    Time: 0 ms (100%), Space: 9.7 MB (38.87%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int M = 1e9 + 7;
+    // recursion and memoization
     int f(int n, vector<int> &prev, vector<int>& dp){
         if(n == 0) return 1;
         if(dp[n] != -1) return dp[n];
@@ -25,6 +26,18 @@ public:
             lastseen[idx] = i;
         }
         vector<int> dp(n+1, -1);
-        return (f(n, prev, dp)-1 + M) % M;
+
+        dp[0] = 1;
+        for(int i=1; i<=n; i++){
+            int total = (2* dp[i-1]) % M;
+
+            if(prev[i] != 0){
+                int duplicate = dp[prev[i] - 1];
+                total = (total - duplicate + M) % M;
+            }
+
+            dp[i] = total;
+        }
+        return (dp[n] - 1 + M) % M;
     }
 };
